a4fdff20b3dc2cffe3e1f677e8b0c850274a8b12
Adam Joyce <adamjoyce@outlook.com>
2017-09-09 23:20:20 +0100

    WIP - Visual indicator for firing direction

--- lacerate/Assets/Scripts/PlayerController.cs
+++ lacerate/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,9 @@
 public class PlayerController : MonoBehaviour 
 {
     private Rigidbody2D rb;                 // The rigidbody attached to the player sprite.
+    private Transform projectionIndicator;  // The indicator used to show the predicted direction and magnitude of the player.
     private Vector2 firstTouchPosition;     // The position of the finger when it touches the screen.
+    private Vector2 startPosInWorld;        // The first touch position in world space.
     private Vector2 direction;              // The direction the player will be fired.
     private bool directionChosen = false;   // True when the player has chosen a direction to fire.
 
@@ -11,6 +13,7 @@ public class PlayerController : MonoBehaviour
 	private void Start() 
 	{
         rb = GetComponent<Rigidbody2D>();
+        projectionIndicator = transform.Find("ProjectionIndicator").transform;
         firstTouchPosition = new Vector2();
         direction = new Vector2();
 	}
@@ -41,10 +44,8 @@ public class PlayerController : MonoBehaviour
         else if (directionChosen)
         {
             //float fireStrength = (direction.magnitude - 0) / (Screen.width - 0) * (300 - 1) + 1;
-            rb.AddForce(-direction * 5); //* fireStrength);
+            rb.AddForce(-direction * 5);
             directionChosen = false;
-
-            Debug.Log(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)));
         }
 	}
 }
